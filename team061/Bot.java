18cb80e32594834bd4556b363c302a757e51b111
Nate Foss <npfoss@gmail.com>
2016-01-05 12:35:27 -0500

    add package and rc

---
+++ team061/Bot.java
@@ -0,0 +1,11 @@
+package team061;
+
+import battlecode.common.*;
+
+public class Bot {
+    public static RobotController rc;
+    protected static Team us;
+    protected static Team them;
+
+    
+}
