9c9feab6dfa408269a27a3bdd3083ff79aa3863c
Eli Lifland <2016elifland@tjhsst.edu>
2016-01-07 14:09:08 -0500

    small change in turret

--- team061/BotTurret.java
+++ team061/BotTurret.java
@@ -29,29 +29,31 @@ public class BotTurret extends Bot {
 			Combat.shootAtNearbyEnemies();
 			if (rc.isWeaponReady()) {
 				int[] indicesOfTargetSignals = getIndicesOfTargetSignals(signals);
-				int numTargetSignals = indicesOfTargetSignals[indicesOfTargetSignals.length-1];
-				MapLocation[] hostileLocations = new MapLocation[numTargetSignals];
-				int[] healths = new int[numTargetSignals];
-				RobotType[] hostileTypes = new RobotType[numTargetSignals];
-				for (int i = 0; i < numTargetSignals; i++) {
-					int[] message = signals[indicesOfTargetSignals[i]].getMessage();
-					int[] decodedMessage = MessageEncode.TURRET_TARGET.decode(message);
-					healths[i] = decodedMessage[0];
-					hostileTypes[i] = RobotType.values()[decodedMessage[1]];
-					hostileLocations[i] = new MapLocation(decodedMessage[2], decodedMessage[3]);
+				if (indicesOfTargetSignals.length > 0) {
+					int numTargetSignals = indicesOfTargetSignals[indicesOfTargetSignals.length - 1];
+					MapLocation[] hostileLocations = new MapLocation[numTargetSignals];
+					int[] healths = new int[numTargetSignals];
+					RobotType[] hostileTypes = new RobotType[numTargetSignals];
+					for (int i = 0; i < numTargetSignals; i++) {
+						int[] message = signals[indicesOfTargetSignals[i]].getMessage();
+						int[] decodedMessage = MessageEncode.TURRET_TARGET.decode(message);
+						healths[i] = decodedMessage[0];
+						hostileTypes[i] = RobotType.values()[decodedMessage[1]];
+						hostileLocations[i] = new MapLocation(decodedMessage[2], decodedMessage[3]);
+					}
+					Combat.shootBestEnemyTakingIntoAccountScoutInfo(hostileLocations, healths, hostileTypes);
 				}
-				Combat.shootBestEnemyTakingIntoAccountScoutInfo(hostileLocations, healths, hostileTypes);
 			}
 		}
 	}
 
 	private static int[] getIndicesOfTargetSignals(Signal[] signals) {
-		int[] indicesOfTargetSignals = new int[signals.length+1];
+		int[] indicesOfTargetSignals = new int[signals.length + 1];
 		int count = 0;
 		for (int i = 0; i < signals.length; i++) {
 			int[] message = signals[i].getMessage();
 			MessageEncode msgType = MessageEncode.whichStruct(message[0]);
-			if(msgType == MessageEncode.TURRET_TARGET){
+			if (msgType == MessageEncode.TURRET_TARGET) {
 				indicesOfTargetSignals[count] = i;
 				count++;
 			}
