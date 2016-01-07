75ceb3b0be444370c1f8ac1ba7c2db10de0e106f
Nate Foss <npfoss@gmail.com>
2016-01-07 12:53:04 -0500

    finally fixed messaging! yay

--- team061/MessageEncode.java
+++ team061/MessageEncode.java
@@ -5,12 +5,14 @@ import java.lang.Math.*;
 
 public enum MessageEncode {
 	TURRET_TARGET(0, new int[]{3, 7, 1, 2}, 2),// health, robotType, xloc, yloc
-	PROXIMITY_NOTIFICATION(1, new int[]{4}, 1);// radius squared (max dist to go from archon)
+	PROXIMITY_NOTIFICATION(1, new int[]{4}, 0),// radius squared (max dist to go from archon)
+	ALPHA_ARCHON_LOCATION(2, new int[]{1,2},0);
 	//note: don't forget to add to the whichStruct method when you add more encode keys
 
 	private final int reasonNumber;
 	/* 0 - tell turret where to shoot
 	 * 1 - tell units how close to stay to the alpha archon
+	 * 2 - notify a unit of the alpha archon location
 	 * 
 	 */
 
@@ -29,6 +31,9 @@ public enum MessageEncode {
 	private static final int[] howMuchSpaceDataNeeds = {3, 15, 15, 13, 7, 0, 0, 4, 0, 0};
 	//get 30 slots total per int
 
+	// new method to send the message for you
+	// use Bot.rc to access the rc
+
 	private MessageEncode(int reason, int[] data, int split) {
 		reasonNumber = reason;
 		whichDataToInclude = data;
@@ -38,7 +43,7 @@ public enum MessageEncode {
 	 * Returns the two ints to send in your message. 
 	 * 
 	 * Use by saying (for example):
-	 * 		MessageEncode.TURRET_TARGET.encode(new int[]{87, targettype.ordinal(), 1453, 1234);
+	 * 		MessageEncode.TURRET_TARGET.encode(new int[]{87, targettype.ordinal(), 1453, 1234});
 	 * 								   health of the unit ^			^			x loc^	   ^ y loc
 	 * 													  			|		
 	 * 				the ordinal is really important whenever you send RobotTypes to this
@@ -48,19 +53,19 @@ public enum MessageEncode {
 	public int[] encode(int[] data) {
 		int[] mess = new int[2];
 		mess[0] = reasonNumber;
-		int powerOfTwo = howMuchSpaceDataNeeds[0] - 1;
+		int powerOfTwo = multiplyByTwo(1,howMuchSpaceDataNeeds[0]);
 		for ( int i = 0; i < whereToSplitData; i++){
-			powerOfTwo += howMuchSpaceDataNeeds[whichDataToInclude[i]];
 			if(whichDataToInclude[i] == 1 || whichDataToInclude[i] == 2)	
 				data[i] += 16000;
-			mess[0] += data[i]*(int)(Math.pow(2, powerOfTwo)+.01);
+			mess[0] += data[i]*powerOfTwo;
+			powerOfTwo = multiplyByTwo(powerOfTwo, howMuchSpaceDataNeeds[whichDataToInclude[i]]);
 		}
-		powerOfTwo = -1;
+		powerOfTwo = 1;
 		for ( int i = whereToSplitData ; i < whichDataToInclude.length ; i++){
-			powerOfTwo += howMuchSpaceDataNeeds[whichDataToInclude[i]];
 			if(whichDataToInclude[i] == 1 || whichDataToInclude[i] == 2)	
 				data[i] += 16000;
-			mess[1] += data[i]*(int)(Math.pow(2, powerOfTwo)+.01);
+			mess[1] += data[i]*powerOfTwo;
+			powerOfTwo = multiplyByTwo(powerOfTwo, howMuchSpaceDataNeeds[whichDataToInclude[i]]);
 		}
 		return mess;
 	}
@@ -72,19 +77,20 @@ public enum MessageEncode {
 	 */
 	public int[] decode(int[] mess){
 		int[] data = new int[whichDataToInclude.length];
-		int powerOfTwo = howMuchSpaceDataNeeds[0] - 1;
+		int powerOfTwo = multiplyByTwo(1, howMuchSpaceDataNeeds[0]);
 		for ( int i = 0; i < whereToSplitData; i++){
-			powerOfTwo += howMuchSpaceDataNeeds[whichDataToInclude[i]];
-			data[i] = mess[0]/(int)(Math.pow(2, powerOfTwo)+.01) % howMuchSpaceDataNeeds[whichDataToInclude[i]];
+			System.out.println(Math.log(powerOfTwo)/Math.log(2) + " " + Math.log(multiplyByTwo(powerOfTwo, howMuchSpaceDataNeeds[whichDataToInclude[i]]))/Math.log(2));
+			data[i] = mess[0]/powerOfTwo % multiplyByTwo(1, howMuchSpaceDataNeeds[whichDataToInclude[i]]);
 			if(whichDataToInclude[i] == 1 || whichDataToInclude[i] == 2)	
 				data[i] -= 16000;
+			powerOfTwo = multiplyByTwo(powerOfTwo, howMuchSpaceDataNeeds[whichDataToInclude[i]]);
 		}
-		powerOfTwo = -1;
+		powerOfTwo = 1;
 		for ( int i = whereToSplitData ; i < whichDataToInclude.length ; i++){
-			powerOfTwo += howMuchSpaceDataNeeds[whichDataToInclude[i]];
-			data[i] = mess[1]/(int)(Math.pow(2, powerOfTwo)+.01) % howMuchSpaceDataNeeds[whichDataToInclude[i]];
+			data[i] = mess[1]/powerOfTwo % multiplyByTwo(1, howMuchSpaceDataNeeds[whichDataToInclude[i]]);
 			if(whichDataToInclude[i] == 1 || whichDataToInclude[i] == 2)	
 				data[i] -= 16000;
+			powerOfTwo = multiplyByTwo(powerOfTwo, howMuchSpaceDataNeeds[whichDataToInclude[i]]);
 		}
 		return data;
 	}
@@ -100,8 +106,14 @@ public enum MessageEncode {
 		switch(firstInt%(int)(Math.pow(2,howMuchSpaceDataNeeds[0])+.01)){
 		case 0: return TURRET_TARGET;
 		case 1: return PROXIMITY_NOTIFICATION;
-		
+		case 2: return ALPHA_ARCHON_LOCATION;
+
 		default: return null;
 		}
+	}
+	private int multiplyByTwo(int num, int times){
+		for (int j = 0; j < times; j++)
+			num *= 2;
+		return num;
 	}
 }
