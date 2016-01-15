6eb40b2abd6f49c524c76b28fbbf23e92b3b9b4a
Nate Foss <npfoss@gmail.com>
2016-01-15 09:49:00 -0500

    OCD spacing adjustment in Bot

--- OrganizedBot/Bot.java
+++ OrganizedBot/Bot.java
@@ -1,17 +1,15 @@
 package Battlecode2016.OrganizedBot;
 
 import java.util.*;
-
 import java.lang.Math.*;
-
 import battlecode.common.*;
 
 public class Bot {
 	public static RobotController rc;
 	protected static Team us;
 	protected static Team them;
-	protected static MapLocation here; // bot classes are responsible for
-										// keeping this up to date
+	protected static MapLocation here; 	// bot classes are responsible for
+									   	// keeping this up to date
 	protected static Random rand;
 	protected static Direction[] directions = { Direction.NORTH, Direction.NORTH_EAST, Direction.EAST,
 			Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST };
