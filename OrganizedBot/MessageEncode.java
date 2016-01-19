f1921714df5d008de4882795c02a0b3c38beec79
Nate Foss <npfoss@gmail.com>
2016-01-19 13:00:07 -0500

    add two more message types

--- OrganizedBot/MessageEncode.java
+++ OrganizedBot/MessageEncode.java
@@ -10,7 +10,9 @@ public enum MessageEncode { // NEW OPTIMIZE THIS IF YOU CAN, ALSO LOOK IN BOT CL
 	DIRECT_MOBILE_ARCHON  (4, new int[]{1,2},0),
 	STOP_BEING_MOBILE	  (5, new int[]{1,2},0),
 	MULTIPLE_TARGETS	  (6, new int[]{7,8,7,8,7,8,7,8,7,8}, 5),// 5 map locations (as ints) **x and y offset from sender must be <16
-	WARN_ABOUT_TURRETS    (7, new int[]{7,8,7,8,7,8,7,8,7,8}, 5);// 5 map locations of enemy turrets -- (-1,-1) if fewer than 5
+	WARN_ABOUT_TURRETS    (7, new int[]{7,8,7,8,7,8,7,8,7,8}, 5),// 5 map locations of enemy turrets -- (-1,-1) if fewer than 5
+	PART_OR_NEUTRAL_NOTIF (8, new int[]{1,2},0),// map location of parts/neutral thing
+	ENEMY_ARMY_NOTIF	  (9, new int[]{1,2},0);// map location of centroid
 	//SCOUT_CHECKIN(4, new int[]{    }, 2),
 	//FOUND_PARTS(4, new int[]{5, 1, 2}, 1),		// num parts, xloc, yloc
 	//FOUND_DEN(5, new int[]{1,2},0),				// xloc, ylo
@@ -31,8 +33,10 @@ public enum MessageEncode { // NEW OPTIMIZE THIS IF YOU CAN, ALSO LOOK IN BOT CL
 	 * 5 - tell the mobile archon to turtle (and where to do so)
 	 * 6 - give turrets more than one target
 	 * 7 - warn soldiers to avoid turrets they can't see -- (-1,-1) if fewer than 5
+	 * 8 - scouts to tell archons about parts or neutrals to be interested in
+	 * 9 - for when scouts see a lot of enemies that aren't turrets
 	 * 
-	 * (if you increase the max number (7), make sure the space below matches)
+	 * (if you increase the max number (15), make sure the space below matches)
 	 */
 
 	/* data number values
@@ -50,7 +54,7 @@ public enum MessageEncode { // NEW OPTIMIZE THIS IF YOU CAN, ALSO LOOK IN BOT CL
 	 */
 	private final int[] whichDataToInclude;
 	private final int whereToSplitData; // index in whichDataToInclude that gets bumped to 2nd int
-	private static final int[] howMuchSpaceDataNeeds = {3, 7, 7, 11, 7, 10, 4, 5, 5};
+	private static final int[] howMuchSpaceDataNeeds = {4, 7, 7, 11, 7, 10, 4, 5, 5};
 	//get 30 slots total per int
 
 	// TODO: make "yell" method to do the actual broadcast too
@@ -159,6 +163,8 @@ public enum MessageEncode { // NEW OPTIMIZE THIS IF YOU CAN, ALSO LOOK IN BOT CL
 		case 5: return STOP_BEING_MOBILE;
 		case 6: return MULTIPLE_TARGETS;
 		case 7: return WARN_ABOUT_TURRETS;
+		case 8: return PART_OR_NEUTRAL_NOTIF;
+		case 9: return ENEMY_ARMY_NOTIF;
 
 		default: return null;
 		}
@@ -178,6 +184,8 @@ public enum MessageEncode { // NEW OPTIMIZE THIS IF YOU CAN, ALSO LOOK IN BOT CL
 		case 5: return "STOP_BEING_MOBILE";
 		case 6: return "MULTIPLE_TARGETS";
 		case 7: return "WARN_ABOUT_TURRETS";
+		case 8: return "PART_OR_NEUTRAL_NOTIF";
+		case 9: return "ENEMY_ARMY_NOTIF";
 
 		default: return "@Nate update the toString you idiot";
 		}
