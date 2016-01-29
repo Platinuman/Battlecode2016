313644ceb5b0df53525325d5850c37d22063d397
Eli Lifland <2016elifland@tjhsst.edu>
2016-01-28 23:34:53 -0500

    ugh

--- ARMAGEDDONBOT/Harass.java
+++ ARMAGEDDONBOT/Harass.java
@@ -482,19 +482,19 @@ public class Harass extends Bot {
 //			targetLoc = archonLoc;
 //			return;
 //		}
-		int fate = rand.nextInt(1000);
-		if(huntingDen && numDensToHunt > 1 && fate < 100 && !Util.isSurrounded(here) && rc.canSenseLocation(targetLoc) && rc.senseNearbyRobots(targetLoc, type.attackRadiusSquared, us).length > 15){
-			rc.setIndicatorString(2, "moving on");
-			targetDens[bestIndex] = null;
-			numDensToHunt--;
-			bestIndex = Util.closestLocation(targetDens, here, targetDenSize);
-			targetLoc = targetDens[bestIndex];
-		}
-		
-		//if(huntingDen){
+		//int fate = rand.nextInt(1000);
+		//if(huntingDen && numDensToHunt > 1 && fate < 100 && !Util.isSurrounded(here) && rc.canSenseLocation(targetLoc) && rc.senseNearbyRobots(targetLoc, type.attackRadiusSquared, us).length > 15){
+		//	rc.setIndicatorString(2, "moving on");
+		//	targetDens[bestIndex] = null;
+		//	numDensToHunt--;
 		//	bestIndex = Util.closestLocation(targetDens, here, targetDenSize);
 		//	targetLoc = targetDens[bestIndex];
 		//}
+		
+		if(huntingDen && numDensToHunt > 1 && rc.isCoreReady()){
+			bestIndex = Util.closestLocation(targetDens, here, targetDenSize);
+			targetLoc = targetDens[bestIndex];
+		}
 		
 //		if(targetLoc == null){
 //			//targetLoc = archonLoc;
